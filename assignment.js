4b753c1a33f042b576cb4fb25a2985ca101d3f40
ruhul rabby <ruhulalam1994@gmail.com>
2021-01-22 03:55:37 +0600

    solved three problem

--- assignment.js
+++ assignment.js
@@ -1,1 +1,49 @@
-// assignment no.3 javascript
+// https://github.com/ruhulrabby/assignmentThree
+
+// assignment task 1 
+
+function kilometerToMeter (kilometer) {
+    let meter = kilometer * 1000;
+    return meter;
+}
+
+//******************* */
+
+//assignment task 2 
+
+function budgetCalculator (watch, phone, laptop) {
+    let sumOfWatch = watch * 50;
+    let sumOfPhone = phone * 100;
+    let sumOfLaptop = laptop * 500;
+    let total = sumOfWatch + sumOfPhone + sumOfLaptop;
+    return total;
+}
+
+/**********************/
+
+// assignment task 3
+
+function hotelCost(stay) {
+    var totalCost = 0;
+    if (stay <= 10 ) {
+        totalCost = stay * 100;
+    }
+    else if (stay <= 20) {
+        var firstTenDays = 10 * 100;
+        var remaining = stay - 10;
+        var secondTenDays = remaining * 80;
+         totalCost = firstTenDays + secondTenDays;
+    }
+    else {
+        var firstTenDays = 10 * 100;
+        var secondTenDays = 10 * 80;
+        var remainingDays = stay - 20;
+        var nextFewDays = remainingDays * 50;
+        totalCost = firstTenDays + secondTenDays + nextFewDays;
+    }
+    return totalCost;
+}
+
+    //***************** */
+
+    // assignment task 4
